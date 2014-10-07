53434c30f0249ea85651ee166cff5c4cb41eac62
BHolmwood <bholmwoo@ualberta.ca>
2014-10-07 14:36:41 -0600

    Added skeletal project and test project.

---
+++ Funtime-Runtime-App/gen/ca/ualberta/cs/funtime_runtime/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ca.ualberta.cs.funtime_runtime;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Funtime-Runtime-testTest/gen/ca/ualberta/cs/funtime_runtime/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ca.ualberta.cs.funtime_runtime.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
