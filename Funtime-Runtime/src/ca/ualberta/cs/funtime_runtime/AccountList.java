944fcf85bbc1a8a938aa1b97cdde254836a5c131
BHolmwood <bholmwoo@ualberta.ca>
2014-10-17 17:18:57 -0600

    Added a sort test

---
+++ Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/AccountList.java
@@ -0,0 +1,27 @@
+package ca.ualberta.cs.funtime_runtime;
+
+
+public class AccountList{
+
+	public AccountList(){
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	public void add(Account account) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Account get(int i)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/Sort.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.funtime_runtime;
+
+public class Sort{
+
+	public QuestionList sortByDate(QuestionList questionList) {	
+		return null;
+	}
+	
+	public QuestionList sortByPicture(QuestionList questionList) {
+		return null;
+	}
+	
+}
+
