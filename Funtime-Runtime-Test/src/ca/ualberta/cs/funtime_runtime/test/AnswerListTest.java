8f8e19740352983efff7a55926305c6e69dcfc99
bsmolley <bsmolley@ualberta.ca>
2014-10-17 13:17:33 -0600

    Added AnswerList JUnit

---
+++ Funtime-Runtime-Test/src/ca/ualberta/cs/funtime_runtime/test/AnswerListTest.java
@@ -0,0 +1,34 @@
+package ca.ualberta.cs.funtime_runtime.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ca.ualberta.cs.funtime_runtime.AnswerList;
+import ca.ualberta.cs.funtime_runtime.AuthorAnswerActivity;
+
+public class AnswerListTest extends ActivityInstrumentationTestCase2<AuthorAnswerActivity> {
+
+	public AnswerListTest() {
+		super(AuthorAnswerActivity.class);
+	}
+	
+	public void testMakeList(){
+		AnswerList answerList = new AnswerList();
+		assertNull(answerList);
+	}
+	
+	public void testAddAnswer(){
+		AnswerList answerList = new AnswerList();
+		answerList.add("test one");
+		answerList.add("test two");
+		assertNotNull(answerList);
+		
+	}
+	
+	public void testRemoveAnswer(){
+		AnswerList answerList = new AnswerList();
+		answerList.add("test one");
+		assertNotNull(answerList);
+		answerList.remove("test one");
+		assertNull(answerList);
+		
+	}
+};
--- Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/AnswerList.java
+++ Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/AnswerList.java
@@ -1,9 +1,21 @@
 package ca.ualberta.cs.funtime_runtime;
 
-public class AnswerList {
 
-	public AnswerList() {
+public class AnswerList{
+
+	public AnswerList(){
+		
 		// TODO Auto-generated constructor stub
+	}
+
+	public void add(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void remove(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
