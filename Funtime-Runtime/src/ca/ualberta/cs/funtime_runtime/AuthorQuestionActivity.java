c7fef8574f3614b60f1086acce315e71a431aa85
deborsi <deborsi@ualberta.ca>
2014-10-27 01:33:38 -0600

    Added functional menu to the AuthorQuestion page

--- Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/AuthorQuestionActivity.java
+++ Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/AuthorQuestionActivity.java
@@ -1,10 +1,13 @@
 package ca.ualberta.cs.funtime_runtime;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AuthorQuestionActivity extends Activity{
 	
@@ -36,6 +39,75 @@ public class AuthorQuestionActivity extends Activity{
 		getMenuInflater().inflate(R.menu.author_question, menu);
 		return true;
 	}
+	
+	 @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// switch case to choose an item from the menu
+			
+	//-------------------------------------------
+	// Menu Items Switch Case
+	//-------------------------------------------
+		 switch (item.getItemId()) {
+		 	case R.id.my_questions_menu_item:
+		 		openMyQuestions();
+		 		return true;
+			case R.id.my_answers_menu_item:
+				openMyAnswers();
+				return true;
+			case R.id.my_favorites_menu_item:
+				openMyFavourites();
+				return true;
+			case R.id.my_reading_list_item:
+				openMyReadingList();
+				return true;
+			case R.id.my_history_list_item:
+				openMyHistory();
+				return true;
+			case R.id.sort_list_item:
+				openSortList();
+				return true;
+			default:
+				return true;
+			}	
+		}
+		
+		public void openMyQuestions() {
+			Toast.makeText(this, "My Questions", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(AuthorQuestionActivity.this, MyQuestionsActivity.class);
+			startActivity(intent);
+		}
+		
+		public void openMyAnswers() {
+			Toast.makeText(this, "My Answers", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(AuthorQuestionActivity.this, MyAnswersActivity.class);
+			startActivity(intent);
+		}
+		
+		public void openMyFavourites() {
+			Toast.makeText(this, "My Favourites", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(AuthorQuestionActivity.this, MyFavouritesActivity.class);
+			startActivity(intent);
+		}
+		
+		public void openMyReadingList() {
+			Toast.makeText(this, "My Reading List", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(AuthorQuestionActivity.this, ReadingListActivity.class);
+			startActivity(intent);
+		}
+		
+		public void openMyHistory() {
+			Toast.makeText(this, "My History", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(AuthorQuestionActivity.this, MyHistoryActivity.class);
+			startActivity(intent);
+		}
+
+		public void openSortList() {
+			Toast.makeText(this, "Choose A Sorting Method", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(AuthorQuestionActivity.this, Sort.class);
+			startActivity(intent);
+		}
+//-------------------------------------------
+//-------------------------------------------
 	
 	public Question submitQuestion() {
 		//Question question = new Question(questionTitle, questionBody, "username");
