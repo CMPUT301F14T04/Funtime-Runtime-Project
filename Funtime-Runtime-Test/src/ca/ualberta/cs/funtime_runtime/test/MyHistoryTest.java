0cb0d1c5899816659090f1f5ee442bdc2c530043
Steven Cherfan <cherfan@ualberta.ca>
2014-11-06 11:34:54 -0700

    Test runs and passes, changed coding style to match project.

--- Funtime-Runtime-Test/src/ca/ualberta/cs/funtime_runtime/test/MyHistoryTest.java
+++ Funtime-Runtime-Test/src/ca/ualberta/cs/funtime_runtime/test/MyHistoryTest.java
@@ -1,7 +1,4 @@
-
 package ca.ualberta.cs.funtime_runtime.test;
-
-import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.Instrumentation;
@@ -12,35 +9,20 @@ import ca.ualberta.cs.funtime_runtime.MyHistoryActivity;
 import ca.ualberta.cs.funtime_runtime.Question;
 
 public class MyHistoryTest extends
-        ActivityInstrumentationTestCase2<MyHistoryActivity>
-{
+	ActivityInstrumentationTestCase2<MyHistoryActivity> {
 
-    Instrumentation instrumentation;
-    Activity activity;
-    Account account;
-
-    public MyHistoryTest()
-    {
-        super(MyHistoryActivity.class);
+    public MyHistoryTest() {
+	super(MyHistoryActivity.class);
     }
 
-    protected void setUp() throws Exception
-    {
-        super.setUp();
-        instrumentation = getInstrumentation();
-        activity = getActivity();
+    public void testAddQuestion() {
+	 Account account = new Account("TestUser1");
+	ApplicationState.setAccount(account);
 
-    }
-
-    public void testAddQuestion()
-    {
-        account = new Account("TestUser1");
-        ApplicationState.setAccount(account);
-
-        Question question1 = new Question("What is the meaning of life?",
-                "body 1 test", "user1");
-        account.addToHistory(question1);
-        //assertNotNull(historyList);
+	Question question = new Question("What is the meaning of life?",
+		"body 1 test", "user1");
+	account.addToHistory(question);
+	assertNotNull(account.getHistoryList());
     }
 
 }
--- Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/MyHistoryActivity.java
+++ Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/MyHistoryActivity.java
@@ -1,4 +1,3 @@
-
 package ca.ualberta.cs.funtime_runtime;
 
 import java.util.ArrayList;
@@ -24,139 +23,135 @@ public class MyHistoryActivity extends CustomActivity {
     QuestionListAdapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_history);
 
-        ActionBar actionbar = getActionBar();
-		actionbar.setDisplayHomeAsUpEnabled(true);
-        
-        myHistoryListView = (ListView) findViewById(R.id.myHistory_ListView);
-        account = ApplicationState.getAccount();
+	ActionBar actionbar = getActionBar();
+	actionbar.setDisplayHomeAsUpEnabled(true);
 
-        myHistoryList = account.getHistoryList();
+	myHistoryListView = (ListView) findViewById(R.id.myHistory_ListView);
+	account = ApplicationState.getAccount();
 
-        ArrayList<Question> historyList = new ArrayList<Question>();
-        historyList = myHistoryList;
-        adapter = new QuestionListAdapter(this, R.layout.question_list_adapter2,
-                historyList);
-        myHistoryListView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
+	myHistoryList = account.getHistoryList();
 
-        myHistoryListView.setOnItemClickListener(new OnItemClickListener()
-        {
+	ArrayList<Question> historyList = new ArrayList<Question>();
+	historyList = myHistoryList;
+	adapter = new QuestionListAdapter(this,
+		R.layout.question_list_adapter2, historyList);
+	myHistoryListView.setAdapter(adapter);
+	adapter.notifyDataSetChanged();
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                    int position, long id)
-            {
-                Question question = (Question) adapter.getItem(position);
-                Bundle bundle = new Bundle();
-                bundle.putSerializable("Question", question);
-                Intent intent = new Intent(MyHistoryActivity.this,
-                        QuestionPageActivity.class);
-                intent.putExtras(bundle);
-                
-                ApplicationState.setPassableQuestion(question);
-                
-                startActivity(intent);
+	myHistoryListView.setOnItemClickListener(new OnItemClickListener() {
 
-            }
-        });
-        
-		registerForContextMenu(myHistoryListView);
-        
+	    @Override
+	    public void onItemClick(AdapterView<?> parent, View view,
+		    int position, long id) {
+		Question question = (Question) adapter.getItem(position);
+		Bundle bundle = new Bundle();
+		bundle.putSerializable("Question", question);
+		Intent intent = new Intent(MyHistoryActivity.this,
+			QuestionPageActivity.class);
+		intent.putExtras(bundle);
+
+		ApplicationState.setPassableQuestion(question);
+
+		startActivity(intent);
+
+	    }
+	});
+
+	registerForContextMenu(myHistoryListView);
+
     }
-    
-	@Override
-	public void onRestart() {
-		super.onRestart();
-		adapter.notifyDataSetChanged();
-	
+
+    @Override
+    public void onRestart() {
+	super.onRestart();
+	adapter.notifyDataSetChanged();
+
+    }
+
+    // Adapted from
+    // http://www.mikeplate.com/2010/01/21/show-a-context-menu-for-long-clicks-in-an-android-listview/
+    // 2014-09-21
+    public void onCreateContextMenu(ContextMenu menu, View v,
+	    ContextMenuInfo menuInfo) {
+	super.onCreateContextMenu(menu, v, menuInfo);
+	AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
+	Question selectedQuestion = adapter.getItem(info.position);
+
+	if (account.getReadingList().contains(selectedQuestion)) {
+	    menu.add("Remove from reading list");
+	} else {
+	    menu.add("Add to reading list");
+	}
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+
+	AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+		.getMenuInfo();
+	int itemIndex = info.position;
+	Question selectedQuestion = adapter.getItem(itemIndex);
+
+	if (item.getTitle() == "Remove from reading list") {
+	    account.removeReadLater(selectedQuestion);
+	} else if (item.getTitle() == "Add to reading list") {
+	    account.readLater(selectedQuestion);
+
+	} else {
+	    return false;
 	}
 
-	// Adapted from http://www.mikeplate.com/2010/01/21/show-a-context-menu-for-long-clicks-in-an-android-listview/ 2014-09-21
-	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-	    super.onCreateContextMenu(menu, v, menuInfo);
-	    AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
-	    Question selectedQuestion = adapter.getItem(info.position);
-	    
-	    if (account.getReadingList().contains(selectedQuestion)) {
-	    	menu.add("Remove from reading list");
-	    } else {
-	    	menu.add("Add to reading list");
-	    }
-	}
+	return true;
 
-	public boolean onContextItemSelected(MenuItem item) {
+    }
 
-	    AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-	    int itemIndex = info.position;
-	    Question selectedQuestion = adapter.getItem(itemIndex);
-	    
-		if (item.getTitle() == "Remove from reading list") {
-		    account.removeReadLater(selectedQuestion);
-		}
-		else if (item.getTitle() == "Add to reading list") {
-		    account.readLater(selectedQuestion);
-            
-	    } 
-		else {
-	        return false;
-	    }
-		
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.history, menu);
+	return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	// switch case to choose an item from the menu
+
+	// -------------------------------------------
+	// Menu Items Switch Case
+	// -------------------------------------------
+	switch (item.getItemId()) {
+	case android.R.id.home:
+	    openMyHome();
 	    return true;
-
+	case R.id.home_menu_item:
+	    openMyHome();
+	    return true;
+	case R.id.login_menu_item:
+	    openLogin();
+	    return true;
+	case R.id.my_questions_menu_item:
+	    openMyQuestions();
+	    return true;
+	case R.id.my_answers_menu_item:
+	    openMyAnswers();
+	    return true;
+	case R.id.my_favorites_menu_item:
+	    openMyFavourites();
+	    return true;
+	case R.id.my_reading_list_item:
+	    openMyReadingList();
+	    return true;
+	case R.id.sort_list_item:
+	    openSortList();
+	    return true;
+	default:
+	    return true;
 	}
-	
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.history, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // switch case to choose an item from the menu
-
-        // -------------------------------------------
-        // Menu Items Switch Case
-        // -------------------------------------------
-        switch (item.getItemId())
-        {
-        	case android.R.id.home:
-				openMyHome();
-				return true;
-        	case R.id.home_menu_item:
-        		openMyHome();
-        		return true;
-            case R.id.login_menu_item:
-                openLogin();
-                return true;
-            case R.id.my_questions_menu_item:
-                openMyQuestions();
-                return true;
-            case R.id.my_answers_menu_item:
-                openMyAnswers();
-                return true;
-            case R.id.my_favorites_menu_item:
-                openMyFavourites();
-                return true;
-            case R.id.my_reading_list_item:
-                openMyReadingList();
-                return true;
-            case R.id.sort_list_item:
-                openSortList();
-                return true;
-            default:
-                return true;
-        }
     }
 
     // -------------------------------------------
