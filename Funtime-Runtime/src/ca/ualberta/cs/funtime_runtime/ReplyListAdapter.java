98129c5c0a138ea0c3dfeaaba8fcde6af7811e44
Pranjali Pokharel <pranjali@ualberta.ca>
2014-11-07 12:11:17 -0700

    Added javadocs for ReplyListAdapter

--- Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/ReplyListAdapter.java
+++ Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/ReplyListAdapter.java
@@ -10,6 +10,13 @@ import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+/**
+ * This class is a controller class that displays the various reply list items in proper format
+ * It displays all the components associated with a Reply object 
+ * Reply, username of author and date are displayed in the list for each reply using this adapter
+ * 
+ * @author Pranjali Pokharel
+ */
 public class ReplyListAdapter extends ArrayAdapter<Reply> {
 
 	Context ctx;
@@ -18,6 +25,13 @@ public class ReplyListAdapter extends ArrayAdapter<Reply> {
 	int res;
 	LayoutInflater inflater;
 
+	/**
+	 * This function initializes the adapter. 
+	 * The objects being displayed on screen are Array List Reply objects
+	 * @param context
+	 * @param resource
+	 * @param objects
+	 */
 	public ReplyListAdapter(Context context, int resource, ArrayList<Reply> objects) {
 		super(context, resource, objects);
 		ctx = context;
@@ -26,9 +40,21 @@ public class ReplyListAdapter extends ArrayAdapter<Reply> {
 		inflater = LayoutInflater.from(context);
 	}
 	
-	// Adapted from http://www.survivingwithandroid.com/2013/02/android-listview-adapter-imageview.html -- Accessed Oct 23, 2014
-	// Adapted from http://www.framentos.com/en/android-tutorial/2012/07/16/listview-in-android-using-custom-listadapter-and-viewcache/ 
-	//    -- Accessed Oct 23, 2014
+	/**
+	 * This function gets and displays the view consisting of reply body, username of author and date/time of reply
+	 * A reply object of the list is displayed at the position corresponding to its position in the reply list
+	 * 
+	 * @param position
+	 * @param convertView
+	 * @param parent
+	 * 
+	 * @return theView
+	 * 
+	 * Adapted from http://www.survivingwithandroid.com/2013/02/android-listview-adapter-imageview.html -- Accessed Oct 23, 2014
+	 * Adapted from http://www.framentos.com/en/android-tutorial/2012/07/16/listview-in-android-using-custom-listadapter-and-viewcache/ 
+	 * -- Accessed Oct 23, 2014
+	 */
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		
