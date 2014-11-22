dcd65d0bb1e2688c05f3d8cf9c6d1f2852e53f39
BHolmwood <bholmwoo@ualberta.ca>
2014-11-21 17:00:52 -0700

    Just kidding, that didn't work. This should though.

--- Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/classes/Account.java
+++ Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/classes/Account.java
@@ -1,5 +1,6 @@
 package ca.ualberta.cs.funtime_runtime.classes;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 
@@ -13,7 +14,12 @@ import java.util.ArrayList;
  * @author Benjamin Holmwood
  * 
  */
-public class Account {
+public class Account implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1130304605213585452L;
+	
 	private String username;
 	private ArrayList<Question> favourites;
 	private ArrayList<Question> answeredList;
