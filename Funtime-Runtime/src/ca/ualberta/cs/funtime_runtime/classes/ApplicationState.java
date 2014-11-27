55cf75ba8fc7503ca0dc9fae3187af928579078a
bsmolley <bsmolley@ualberta.ca>
2014-11-27 16:59:29 -0700

    Fixed My pages not updating properly

--- Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/classes/ApplicationState.java
+++ Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/classes/ApplicationState.java
@@ -211,6 +211,7 @@ public class ApplicationState extends Application {
 	public static ArrayList<Question> getQuestionList() {
 		//TODO grab the master question list from the sever before returning
 		//refresh();
+		ApplicationState.loadServerQuestions();
 		return questionList;
 	}
 	
