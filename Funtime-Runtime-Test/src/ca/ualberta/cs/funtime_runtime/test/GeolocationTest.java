309bf96bd5c2ec7a60c25c1af23ae968a8ad96b5
bsmolley <bsmolley@ualberta.ca>
2014-11-29 13:11:06 -0700

    Added geolocation test

---
+++ Funtime-Runtime-Test/src/ca/ualberta/cs/funtime_runtime/test/GeolocationTest.java
@@ -0,0 +1,24 @@
+package ca.ualberta.cs.funtime_runtime.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ca.ualberta.cs.funtime_runtime.SearchActivity;
+import ca.ualberta.cs.funtime_runtime.classes.Geolocation;
+
+public class GeolocationTest extends ActivityInstrumentationTestCase2<SearchActivity> {
+
+	public GeolocationTest() {
+		super(SearchActivity.class);
+	}
+	
+	public void testFindLocation() {
+		Geolocation location = new Geolocation(getActivity());
+		String foundLocation;
+		String actualLocation = "N/A";
+		
+		location.findLocation();
+		foundLocation = location.getLocation();
+		
+		assertEquals(foundLocation, actualLocation);	
+	}
+
+}
