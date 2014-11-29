fffce06184d82abb1227234f5adec1db24cc7ef9
Pranjali Pokharel <pranjali@ualberta.ca>
2014-11-29 15:01:30 -0700

    cleaned up and added more to the thread testing. Still figuring out how to complete it

--- Funtime-Runtime-Test/src/ca/ualberta/cs/funtime_runtime/test/AddAccountThreadTest.java
+++ Funtime-Runtime-Test/src/ca/ualberta/cs/funtime_runtime/test/AddAccountThreadTest.java
@@ -2,7 +2,6 @@ package ca.ualberta.cs.funtime_runtime.test;
 
 import junit.framework.TestCase;
 import ca.ualberta.cs.funtime_runtime.classes.Account;
-import ca.ualberta.cs.funtime_runtime.classes.Answer;
 import ca.ualberta.cs.funtime_runtime.elastic.ESAccountManager;
 
 
@@ -11,6 +10,7 @@ public class AddAccountThreadTest extends TestCase
 
 	public void testAddAccountThread(){
 		Account account = new Account("Test Account Username");
+		account.setId(3456);
 		
 		ESAccountManager manager = new ESAccountManager();
 		
