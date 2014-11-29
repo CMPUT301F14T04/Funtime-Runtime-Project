92a39e392d3509545b6141d5fa9e5399f52731f1
bsmolley <bsmolley@ualberta.ca>
2014-11-28 18:27:33 -0700

    Menu options

--- Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/MyQuestionsActivity.java
+++ Funtime-Runtime/src/ca/ualberta/cs/funtime_runtime/MyQuestionsActivity.java
@@ -157,5 +157,28 @@ public class MyQuestionsActivity extends CustomActivity {
 		return true;
 	}
 	
+	@Override
+	public void sortDate() {
+		sorter.sortByDate();	
+		adapter.notifyDataSetChanged();
+	}
+	
+	@Override
+	public void sortVotes() {
+		sorter.sortByVotes();
+		adapter.notifyDataSetChanged();
+	}
+	
+	@Override
+	public void sortPhoto() {
+		sorter.sortByPhoto();
+		adapter.notifyDataSetChanged();	
+	}
+	
+	@Override
+	public void sortLocation() {
+		sorter.sortByLocation(this);
+		adapter.notifyDataSetChanged();
+	}
 
 }
